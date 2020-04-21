f31f247e1ba23df91eaae79ade3b311a4fb94ef5
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-21 17:57:14 +1200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! " + ("Headers: ", vars(request.headers))
+    return "Hello World! " + convertTuple(vars(request.headers))
+
+def convertTuple(tup): 
+    str =  ''.join(tup) 
+    return str
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
