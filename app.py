a96616249a00fa249cba9cac73a050d8c1165a53
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-21 17:38:16 +1200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! " + print(request.headers[X-Remote-User])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
