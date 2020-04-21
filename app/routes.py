2883643b65ca977bba9fb6eec9cbe63faf12dbc2
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 11:51:53 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,15 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+import teradataSQL as td
+
+def loginToTD(strHost, strUsr, strPwd):
+    try:
+        tdSession = teradatasql.connect(host=strHost, user=strUsr, password=strPwd, logmech='LDAP')
+        return True
+     except:
+        return False
+
 
 @app.route('/')
 @app.route('/index')
@@ -24,7 +33,9 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
+        if loginToTD(form.database.data, form.username.data, form.password.data):
+            flash('Login succeeded for user {}'.format(form.username.data))
+         else:
+            flash('Login failed for user {}'.format(form.username.data))
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
