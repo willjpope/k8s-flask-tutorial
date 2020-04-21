9339158d706904a6afb709f110bc85cf4b2d59e2
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-21 19:11:25 +1200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! " + request.headers
+    return "Hello World! " + str(request.headers)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
