468f2423b9c492b71e457a4b729bcf06d7d7dec1
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 11:34:15 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,10 +4,27 @@ from app.forms import LoginForm
 
 
 @app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
-   form = LoginForm()
-   if form.validate_on_submit():
-      flash('Login requested for user {}'.format(form.username.data))
-      return redirect(url_for('index'))
-   return render_template('index.html',  title='Sign In', form=form)
+    user = {'username': 'Will'}
+    posts = [
+        {
+            'author': {'username': 'Jay'},
+            'body': 'Classic bantz!'
+        },
+        {
+            'author': {'username': 'Anneliese'},
+            'body': 'Wish I was at work!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
