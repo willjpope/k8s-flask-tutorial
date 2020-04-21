9626cd894ece25d237e5e01f10f8623b27cd88c0
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 09:22:26 +1200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,22 @@
 from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data)
+        flash('Login requested for user {}'.format(
+            form.username.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html',  title='Sign In', form=form)
