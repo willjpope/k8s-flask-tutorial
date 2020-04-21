7ecd13c9c73aac276b504354f1e9325bf16da27a
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-21 19:01:05 +1200

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! " + convertTuple(vars(request.headers))
-
-def convertTuple(tup): 
-    str =  ''.join(tup) 
-    return str
+    return "Hello World! " + request.headers.get("X-Remote-User")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
