c03e5c4d345a00dcaa78fcada50620ba9b31f37e
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 09:55:28 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from app import app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
