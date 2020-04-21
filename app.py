3dc86c3cd33104777efdc566230ad11230f3faac
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 09:46:57 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-import routes
+from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
