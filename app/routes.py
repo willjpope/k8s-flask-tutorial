e889c52fdef623f93bdb139a4750d77871e6a203
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 11:56:39 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -35,7 +35,7 @@ def login():
     if form.validate_on_submit():
         if loginToTD(form.database.data, form.username.data, form.password.data):
             flash('Login succeeded for user {}'.format(form.username.data))
-         else:
+        else:
             flash('Login failed for user {}'.format(form.username.data))
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
