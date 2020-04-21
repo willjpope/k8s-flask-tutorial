74637ed2102b365775367562016cf080e63a0032
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 11:19:48 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -9,5 +9,5 @@ def index():
    form = LoginForm()
    if form.validate_on_submit():
       flash('Login requested for user {}'.format(form.username.data))
-       return redirect(url_for('index'))
+      return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
