5a8a6e8bd7535ffbb6fcd5ff4496d0cadf9d0771
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 12:12:03 +1200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+import teradata as td
 
-import teradatasql as td
+#import teradatasql as td
 
 def loginToTD(strHost, strUsr, strPwd):
     try:
-        tdSession = td.connect(host=strHost, user=strUsr, password=strPwd, logmech='LDAP')
+        #tdSession = td.connect(host=strHost, user=strUsr, password=strPwd, logmech='LDAP')
+        tdInfo = td.UdaExec(appName = 'UCBD', version = '0.1', logConsole = True)
+        tdConn = tdInfo.connect(method = 'odbc', driver = 'Teradata', system = strHost, username = strUsr, password = strPwd, authentication = 'LDAP')
         return True
     except:
         return False
