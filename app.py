c92823308a68c675d01aa6eb2138cd6aab844611
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 12:00:32 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from app import app
 import config
 
-from app.models import User, Post
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
