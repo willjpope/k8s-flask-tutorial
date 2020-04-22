11531482c075c309ffb29ec2e51e10ec4c01ce92
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 12:41:11 +1200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,5 @@ from app import app
 import config
 import os
 
-os.system('apt-get install -y unixodbc-dev')
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
