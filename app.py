01c7ef14ba66d0d87547730b50032516dde66e11
willjpope <33146159+willjpope@users.noreply.github.com>
2020-04-22 12:32:19 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from app import app
 import config
-from subprocess import STDOUT, check_call
 import os
 
-check_call(['apt-get', 'install', '-y', 'unixodbc-dev'],
-     stdout=open(os.devnull,'wb'), stderr=STDOUT) 
+os.system('sudo apt-get install -y unixodbc-dev')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
